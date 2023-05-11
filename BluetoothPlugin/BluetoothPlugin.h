3c12f10d8706a3fd53e7ab99e83570db0da533f5
zhaozhentao <qq344696734@gmail.com>
2023-05-11 11:36:52 +0800

    Initial Commit

---
+++ BluetoothPlugin/BluetoothPlugin.h
@@ -0,0 +1,18 @@
+//
+//  BluetoothPlugin.h
+//  BluetoothPlugin
+//
+//  Created by 招振涛 on 2023/5/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BluetoothPlugin.
+FOUNDATION_EXPORT double BluetoothPluginVersionNumber;
+
+//! Project version string for BluetoothPlugin.
+FOUNDATION_EXPORT const unsigned char BluetoothPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BluetoothPlugin/PublicHeader.h>
+
+
